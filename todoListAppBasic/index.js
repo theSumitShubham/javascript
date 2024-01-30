57136bcbd62d7975ff8156e2b8f4938a47382a0b
theSumitShubham <kajuraj5@gmail.com>
2024-01-30 23:40:05 +0530

    h

---
+++ todoListAppBasic/index.js
@@ -0,0 +1,7 @@
+const inputField=document.getElementById("inputField").value;
+const addToDo=document.getElementById("addToDo");
+addToDo.addEventListener("click",()=>{
+    let count=0;
+    console.log(count);
+    count++;
+})
